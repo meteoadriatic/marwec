7c267ad584432a8f7746fd827ec378a25e0593e3
Ivan Toman <toman@meteoadriatic.net>
2017-03-16 22:00:57 +0100

    added TODO file, small code cleanup

--- diff_temp.py
+++ diff_temp.py
@@ -1,10 +1,7 @@
 import pandas as pd
-import numpy as np
 
 df_wrf = pd.read_csv('data/input/Maksimir.wrf.csv', sep='\t', index_col=0)
 df_obs = pd.read_csv('data/input/Maksimir.obs.csv', sep='\t', index_col=0, header=None, names=['timestamp', 'tmp2m'])
-
-# print(df_obs)
 
 df_wrf_temp = df_wrf['tmp2m']
 df_obs_temp = df_obs['tmp2m']
