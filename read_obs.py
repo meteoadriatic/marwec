ef43dde62883d0985063ad0ce1bf38e7cfdad323
Nino Rasic <nrasic@meteomatics.com>
2017-03-21 22:28:46 +0100

    Refactor code, now it can be run on both python 2 and python 3

--- read_obs.py
+++ read_obs.py
@@ -1,7 +1,7 @@
-import urllib.request
+import requests as req
 import xmltodict
-import datetime
-#import os
+import datetime as dt
+import os
 
 '''
 try:
@@ -10,30 +10,50 @@ except:
     pass
 '''
 
-for num in range(00, 24):
-    num = str(num)
-    num = num.zfill(2)
-    url = 'http://vrijeme.hr/tablice/hrvatska_n_' + num + '.xml'
-    response = urllib.request.urlopen(url)
-    data = response.read()
+STATIONS = ["Zagreb-Maksimir"]
+output_path = "data/input/"
+file_name = "Maksimir.obs.csv"
+hours = 24
+
+
+def to_timestamp(dt, epoch=dt.datetime(1970, 1, 1)):
+    td = dt - epoch
+    return (td.microseconds + (td.seconds + td.days * 86400) * 10 ** 6) / 10 ** 6
+
+
+# create directory if doesn't exists
+if not os.path.exists(output_path):
+    print ("Creating directory {}".format(output_path))
+    os.makedirs(output_path)
+
+# fetch weather data
+for hour in range(hours):
+    url = 'http://vrijeme.hr/tablice/hrvatska_n_{hour:02}.xml'.format(hour=hour)
+    response = req.get(url)
+    response.raise_for_status()
+    data = response.content
     doc = xmltodict.parse(data)
 
     current_date = doc['Hrvatska']['DatumTermin']['Datum']
     current_time = doc['Hrvatska']['DatumTermin']['Termin']
     current_date_time = current_date + '_' + current_time
-    current_unix_time = datetime.datetime.strptime(current_date_time, "%d.%m.%Y_%H").timestamp()
+    current_date_time = dt.datetime.strptime(current_date_time, "%d.%m.%Y_%H")
+    current_unix_time = to_timestamp(current_date_time)
     print('date =', current_date, ', time =', current_time, ', timestamp  =', int(current_unix_time))
 
-    locindex = 0
-    while True:
-        locindex = locindex + 1
-        location = doc['Hrvatska']['Grad'][locindex]['GradIme']
-        temperature = doc['Hrvatska']['Grad'][locindex]['Podatci']['Temp']
-        temperature = float(temperature)
-        temperature = temperature + 273.15
-        temperature = "{0:.1f}".format(temperature)
-        if location == 'Zagreb-Maksimir':
-            print('temperature =', temperature)
-            with open("data/input/Maksimir.obs.csv", "a") as myfile:
-                myfile.write(str(int(current_unix_time)) + ',' + str(temperature) + '\n')
-            break
+    dhmz_stations = doc["Hrvatska"]["Grad"]
+    file = output_path + file_name
+    with open(file, "a") as output_file:
+        for station in dhmz_stations:
+            try:
+                if station["GradIme"] in STATIONS:
+                    try:
+                        temperature = float(station["Podatci"]["Temp"])
+                        temperature += 273.15  # convert C to K
+                        print ("temperature = {:1}".format(temperature))
+                        output_file.write("{},{:1}\n".format(current_unix_time, temperature))
+                    except ValueError as e:
+                        print ("Couldn't convert value to float: {}, error: {}".format(temperature, e))
+                        continue
+            except KeyError as e:
+                print (str(e))
