0cefa076b4b497260cbc5130750d928b48ab0f1c
Nino Rasic <nrasic@meteomatics.com>
2017-03-21 22:55:01 +0100

    Remove duplicates and dron NaN

--- diff_temp.py
+++ diff_temp.py
@@ -5,8 +5,11 @@ df_obs = pd.read_csv('data/input/Maksimir.obs.csv', sep=',', index_col=0)
 
 df_wrf_temp = df_wrf['tmp2m']
 df_obs_temp = df_obs['tmp2m']
+df_obs_temp = df_obs_temp.drop_duplicates()
+df_wrf_temp = df_wrf_temp.drop_duplicates()
 
 df_diff_temp = df_wrf_temp - df_obs_temp
+df_diff_temp = df_diff_temp.dropna()
 print(df_diff_temp)
 
 df_diff_temp.to_csv('data/input/Maksimir.diff.csv')
