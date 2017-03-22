17ae5e5613e748213337952643d5805faad50c52
Nino Rasic <nrasic@meteomatics.com>
2017-03-22 20:55:01 +0100

    Remove duplicated indexes

--- diff_temp.py
+++ diff_temp.py
@@ -5,8 +5,8 @@ df_obs = pd.read_csv('data/input/Maksimir.obs.csv', sep=',', index_col=0)
 
 df_wrf_temp = df_wrf['tmp2m']
 df_obs_temp = df_obs['tmp2m']
-df_obs_temp = df_obs_temp.drop_duplicates()
-df_wrf_temp = df_wrf_temp.drop_duplicates()
+df_obs_temp = df_obs_temp.reset_index().groupby(df_obs_temp.index.names).first()
+df_wrf_temp = df_wrf_temp.reset_index().groupby(df_wrf_temp.index.names).first()
 
 df_diff_temp = df_wrf_temp - df_obs_temp
 df_diff_temp = df_diff_temp.dropna()
