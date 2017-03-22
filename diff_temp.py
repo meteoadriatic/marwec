dea3564fad97d27d6970f22e440063c56ab35c40
Ivan Toman <toman@meteoadriatic.net>
2017-03-22 22:58:01 +0100

    added rounding, cleaned code

--- diff_temp.py
+++ diff_temp.py
@@ -8,9 +8,6 @@ df_obs_temp = df_obs['tmp2m']
 df_obs_temp = df_obs_temp.reset_index().groupby(df_obs_temp.index.names).first()
 df_wrf_temp = df_wrf_temp.reset_index().groupby(df_wrf_temp.index.names).first()
 
-df_merged = df_obs_temp.merge(df_wrf_temp, left_index=True, right_index=True)
-print(df_merged)
-
 df_diff_temp = df_wrf_temp - df_obs_temp
 df_diff_temp = df_diff_temp.dropna()
 df_diff_temp = df_diff_temp.round(decimals=1)
