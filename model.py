9365f377098090b06e1d8bed9f2abc735c9f86a7
Ivan Toman <toman@meteoadriatic.net>
2017-03-29 19:09:22 +0200

    first working example of classification, updated data files

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsClassifier
 
 '''
 1) COLLECTING AND PREPARING DATA
@@ -27,49 +28,50 @@ as diffrence bewteen forecasted data by WRF and observed data at synoptic statio
 # Read input files
 df_obs = pd.read_csv('data/input/Maksimir.obs.csv', sep=',', index_col=0)
 df_wrf = pd.read_csv('data/input/Maksimir.wrf.csv', sep=',', index_col=0)
-df_diff = pd.read_csv('data/input/Maksimir.diff.csv', sep=',', index_col=0)
+df_diff_temp = pd.read_csv('data/input/Maksimir.diff.csv', sep=',', index_col=0)
 df_fcst = pd.read_csv('data/input/Maksimir.fcst.csv', sep=',', index_col=0)
 
-# Separate weather type for classification model
-df_obs_weather = df_obs['weathertype']
-
-
+'''
 # Encode weather strings into classification numbers
-#enc = OneHotEncoder()
-#enc.fit(['vedro', 'umjereno oblačno', 'pretežno oblačno'])
+enc = OneHotEncoder()
+enc.fit(['vedro', 'umjereno oblačno', 'pretežno oblačno'])
+'''
 
+'''
 weather_types = {'weather_type': ['vedro', 'umjereno oblačno', 'pretežno oblačno', 'potpuno oblačno']}
 df_weather_types = pd.DataFrame(weather_types, columns=['weather_type'])
 df_weather_codes = pd.get_dummies(df_weather_types['weather_type'])
 df_weather_types_codes = pd.concat([df_weather_types, df_weather_codes], axis=1)
+'''
 
-# what now with that?!
+df_obs = df_obs.dropna()  # why this works at all?
 
-#print(df_weather_types_codes)
-#exit()
+df_obs.replace('vedro', 1, inplace=True)
+df_obs.replace('umjereno oblačno', 2, inplace=True)
+df_obs.replace('pretežno oblačno', 3, inplace=True)
+df_obs.replace('potpuno oblačno', 4, inplace=True)
+# print(df_obs)
 
-# Create new dataframe that has same timestamp in both wrf and temp. diff dataframes
-df_merged_temp = df_diff.merge(df_wrf, left_index=True, right_index=True)
+df_obs_weather = df_obs['weathertype']
+df_obs_weather = df_obs_weather.to_frame()
+# print(df_obs_weather)
 
-# Create new dataframe that has same timestamp in both wrf and obs. weather dataframes
+df_merged_temp = df_diff_temp.merge(df_wrf, left_index=True, right_index=True)
 df_merged_weather = df_obs_weather.merge(df_wrf, left_index=True, right_index=True)
-
-print(df_merged_temp)
-#print(df_merged_temp.ix[:, 0])
+# print(df_merged_weather)
+# exit()
 
 # Build arrays for regression model (temperature error)
 y_train_temp = np.array(df_merged_temp.ix[:, 0])
 df_merged_temp.drop(df_merged_temp.columns[[0]], axis=1, inplace=True)
 X_train_temp = np.array(df_merged_temp)
 y_predict_temp = np.array(df_fcst)
-
 # print(X_train.shape)  # Should return (n1, n2) where n1 = n_samples, n2 = n_features
 # print(y_train.shape)  # Should return (m,) where m = n_solutions, and m = n_samples
 
 # Build arrays for classification model (weather type)
-y_train_weather = np.array(df_merged_weather.ix[:, 0])
-df_merged_weather.drop(df_merged_weather.columns[[0]], axis=1, inplace=True)
-X_train_weather = np.array(df_merged_weather)
+X_train_weather = np.array(df_merged_weather.drop(['weathertype'], 1))
+y_train_weather = np.array(df_merged_weather['weathertype'])
 y_predict_weather = np.array(df_fcst)
 
 
@@ -96,6 +98,10 @@ regression model and feed it with our X_train and y_train numpy arrays
 kneighbor_regression = KNeighborsRegressor(n_neighbors=1)
 kneighbor_regression.fit(X_train_temp, y_train_temp)
 
+# example: KNeighborsClassification
+kneighbor_classifier = KNeighborsClassifier(n_neighbors=1)
+kneighbor_classifier.fit(X_train_weather, y_train_weather)
+
 '''
 4) RETURN PREDICTED RESULTS
 ------------------------------
@@ -103,6 +109,8 @@ kneighbor_regression.fit(X_train_temp, y_train_temp)
 
 '''
 # exit()
-prediction = kneighbor_regression.predict(y_predict_temp)
+prediction_temp = kneighbor_regression.predict(y_predict_temp)
+prediction_weather = kneighbor_classifier.predict(y_predict_weather)
 
-print(prediction)
+print('Estimated temperature error =', prediction_temp)
+print('Estimated weather code =', prediction_weather)
