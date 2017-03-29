6195c289bea038a712dafce909fbb93b5ae6ea50
Ivan Toman <toman@meteoadriatic.net>
2017-03-29 20:41:54 +0200

    code cleanups in diff_temp.py, model.py

--- diff_temp.py
+++ diff_temp.py
@@ -11,6 +11,7 @@ df_wrf_temp = df_wrf_temp.reset_index().groupby(df_wrf_temp.index.names).first()
 df_diff_temp = df_wrf_temp - df_obs_temp
 df_diff_temp = df_diff_temp.dropna()
 df_diff_temp = df_diff_temp.round(decimals=1)
-print(df_diff_temp)
+df_diff_temp = df_diff_temp.rename(columns={'tmp2m': 'tmp2mdiff'})
+# print(df_diff_temp)
 
 df_diff_temp.to_csv('data/input/Maksimir.diff.csv')
--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
+#from sklearn.preprocessing import OneHotEncoder
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -32,12 +32,14 @@ df_diff_temp = pd.read_csv('data/input/Maksimir.diff.csv', sep=',', index_col=0)
 df_fcst = pd.read_csv('data/input/Maksimir.fcst.csv', sep=',', index_col=0)
 
 '''
+# DO WE NEED THIS APPROACH TO CODE WEATHER TYPE?
 # Encode weather strings into classification numbers
 enc = OneHotEncoder()
 enc.fit(['vedro', 'umjereno oblačno', 'pretežno oblačno'])
 '''
 
 '''
+# OR THIS?
 weather_types = {'weather_type': ['vedro', 'umjereno oblačno', 'pretežno oblačno', 'potpuno oblačno']}
 df_weather_types = pd.DataFrame(weather_types, columns=['weather_type'])
 df_weather_codes = pd.get_dummies(df_weather_types['weather_type'])
@@ -46,10 +48,12 @@ df_weather_types_codes = pd.concat([df_weather_types, df_weather_codes], axis=1)
 
 df_obs = df_obs.dropna()  # why this works at all?
 
+# OR, MAYBE THIS SIMPLE STRING TO CODE REPLACE IS GOOD ENOUGH?
 df_obs.replace('vedro', 1, inplace=True)
-df_obs.replace('umjereno oblačno', 2, inplace=True)
-df_obs.replace('pretežno oblačno', 3, inplace=True)
-df_obs.replace('potpuno oblačno', 4, inplace=True)
+df_obs.replace('pretežno vedro', 2, inplace=True)
+df_obs.replace('umjereno oblačno', 3, inplace=True)
+df_obs.replace('pretežno oblačno', 4, inplace=True)
+df_obs.replace('potpuno oblačno', 5, inplace=True)
 # print(df_obs)
 
 df_obs_weather = df_obs['weathertype']
@@ -58,16 +62,12 @@ df_obs_weather = df_obs_weather.to_frame()
 
 df_merged_temp = df_diff_temp.merge(df_wrf, left_index=True, right_index=True)
 df_merged_weather = df_obs_weather.merge(df_wrf, left_index=True, right_index=True)
-# print(df_merged_weather)
-# exit()
+# print(df_merged_temp)
 
 # Build arrays for regression model (temperature error)
-y_train_temp = np.array(df_merged_temp.ix[:, 0])
-df_merged_temp.drop(df_merged_temp.columns[[0]], axis=1, inplace=True)
-X_train_temp = np.array(df_merged_temp)
+X_train_temp = np.array(df_merged_temp.drop(['tmp2mdiff'], 1))
+y_train_temp = np.array(df_merged_temp['tmp2mdiff'])
 y_predict_temp = np.array(df_fcst)
-# print(X_train.shape)  # Should return (n1, n2) where n1 = n_samples, n2 = n_features
-# print(y_train.shape)  # Should return (m,) where m = n_solutions, and m = n_samples
 
 # Build arrays for classification model (weather type)
 X_train_weather = np.array(df_merged_weather.drop(['weathertype'], 1))
@@ -95,21 +95,21 @@ regression model and feed it with our X_train and y_train numpy arrays
 '''
 
 # example: KNeighborsRegression
-kneighbor_regression = KNeighborsRegressor(n_neighbors=1)
+kneighbor_regression = KNeighborsRegressor(n_neighbors=3)
 kneighbor_regression.fit(X_train_temp, y_train_temp)
 
 # example: KNeighborsClassification
-kneighbor_classifier = KNeighborsClassifier(n_neighbors=1)
+kneighbor_classifier = KNeighborsClassifier(n_neighbors=3)
 kneighbor_classifier.fit(X_train_weather, y_train_weather)
+
 
 '''
 4) RETURN PREDICTED RESULTS
 ------------------------------
 
-
 '''
-# exit()
 prediction_temp = kneighbor_regression.predict(y_predict_temp)
+prediction_temp = np.round(prediction_temp, 1)
 prediction_weather = kneighbor_classifier.predict(y_predict_weather)
 
 print('Estimated temperature error =', prediction_temp)
